7bd53ba25ea122cf5a8b27a721350b93fd19fd68
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2024-05-02 14:20:23 +0700

    change model - cải thiện hiệu suất

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def predict_image():
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
--- train_model.py
+++ train_model.py
@@ -2,16 +2,19 @@
 from keras.datasets import cifar10
 import matplotlib.pyplot as plt
 # load data
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-# create a grid of 3x3 images
+# Mỗi hình ảnh được biểu diễn dưới dạng ma trận ba chiều, 
+# với các kích thước là đỏ, lục, lam, chiều rộng và chiều cao
+# tải tập dữ liệu lên 2 mảng train và test 
+(X_train, y_train), (X_test, y_test) = cifar10.load_data() 
+# Tạo lưới hình ảnh 3x3
 for i in range(0, 9):
  plt.subplot(330 + 1 + i)
  plt.imshow(X_train[i])
-# show the plot
+# hiển thị hình ảnh
 plt.show()
 
 
-# Simple CNN model for the CIFAR-10 Dataset
+# CNN model for the CIFAR-10 Dataset
 from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense
@@ -23,25 +26,42 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.utils import to_categorical
 # load data
+# Các giá trị pixel nằm trong khoảng từ 0 đến 255 
+# cho mỗi kênh màu đỏ, xanh lục và xanh lam.
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 # normalize inputs from 0-255 to 0.0-1.0
+# dữ liệu được tải dưới dạng int
+# => phải chuyển nó sang float để thực hiện phép chia.
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train = X_train / 255.0
 X_test = X_test / 255.0
-# one hot encode outputs
+
+# chuyển đổi về dạng one-hot encoding
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 num_classes = y_test.shape[1]
-# Create the model
+
+# Tạo model
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=MaxNorm(3)))
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
 model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=MaxNorm(3)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D())
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D())
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu', kernel_constraint=MaxNorm(3)))
+model.add(Dropout(0.2))
 model.add(Dense(512, activation='relu', kernel_constraint=MaxNorm(3)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
 # Compile model
 epochs = 25
@@ -50,8 +70,9 @@ decay = lrate/epochs
 sgd = SGD(learning_rate=lrate, momentum=0.9, decay=decay, nesterov=False)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 model.summary()
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
+# Huấn luyện mô hình
+model.fit(X_train, y_train, validation_data=(X_test, y_test), 
+          epochs=epochs, batch_size=32)
 model.save("model1_cifar_10epoch.h5")
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
